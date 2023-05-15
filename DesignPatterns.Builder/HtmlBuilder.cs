187fad9001425f8ec7fb6369bd982ba6459297b9
Bartosz Litwa <blitwa@kpmg.pl>
2023-05-15 20:53:45 +0200

    Builder: HtmlBuilder Basic

---
+++ DesignPatterns.Builder/HtmlBuilder.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+            
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrEmpty(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach(var element in Elements)
+            {
+                sb.Append(element.ToStringImpl(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString() => ToStringImpl(0);
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HtmlElement root = new();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = rootName };
+        }
+    }
+
+    public class HtmlBuilderExample
+    {
+        public static void Start(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Child 1");
+            builder.AddChild("li", "Child 2");
+
+            Console.WriteLine(builder);
+        }
+    }
+}
--- DesignPatterns.Builder/Program.cs
+++ DesignPatterns.Builder/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using DesignPatterns.Builder;
+
+HtmlBuilderExample.Start(args);
