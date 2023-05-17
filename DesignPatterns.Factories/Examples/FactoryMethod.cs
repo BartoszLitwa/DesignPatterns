edeccc60c78b6179f68bbca5fcedc3be97791ffc
BartoszLitwa <bartosz.litwa@proton.me>
2023-05-17 22:25:07 +0200

    Factories: Inner Factory

--- DesignPatterns.Factories/Examples/FactoryMethod.cs
+++ DesignPatterns.Factories/Examples/FactoryMethod.cs
@@ -22,6 +22,28 @@ namespace DesignPatterns.Factories.Examples
             => new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
 
         public override string ToString() => $"Point - {nameof(x)}: {x}, {nameof(y)}: {y}";
+
+        public static FactoryNotStatic FactoryNot => new FactoryNotStatic();
+
+        public static Point Origin => new Point(0, 0); // Always new instance
+
+        public static Point OriginOnce = new Point(0, 0); // Only once initialized - better
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y) => new Point(x, y);
+
+            public static Point NewPolarPoint(double rho, double theta)
+                => new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public class FactoryNotStatic
+        {
+            public Point NewCartesianPoint(double x, double y) => new Point(x, y);
+
+            public Point NewPolarPoint(double rho, double theta)
+                => new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
     }
 
     public class PointF
@@ -49,16 +71,24 @@ namespace DesignPatterns.Factories.Examples
         public static void Start(string[] args)
         {
             var point = Point.NewPolarPoint(1.0, Math.PI / 2);
-            Console.WriteLine(point);
-
             var point2 = Point.NewCartesianPoint(1, 6);
+            Console.WriteLine(point);
             Console.WriteLine(point2);
 
             var pointF = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
-            Console.WriteLine(pointF);
-
             var pointF2 = PointFactory.NewCartesianPoint(1, 6);
+            Console.WriteLine(pointF);
             Console.WriteLine(pointF2);
+
+            var pointI = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
+            var pointI2 = Point.Factory.NewCartesianPoint(1, 6);
+            Console.WriteLine(pointI);
+            Console.WriteLine(pointI2);
+
+            var pointIF = Point.FactoryNot.NewPolarPoint(1.0, Math.PI / 2);
+            var pointIF2 = Point.FactoryNot.NewCartesianPoint(1, 6);
+            Console.WriteLine(pointIF);
+            Console.WriteLine(pointIF2);
         }
     }
 }
