989b4a73665ad5f24a4b1a2262f32ccb920b7c25
BartoszLitwa <bartosz.litwa@proton.me>
2023-05-27 20:41:42 +0200

    Composite: Geometric Shapes, Neural Networks

---
+++ DesignPatterns.Composite/Examples/GeometricShapes.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Composite.Examples
+{
+    public class GraphicObject
+    {
+        public string Color;
+        public virtual string Name { get; set; } = "Group";
+
+        private Lazy<List<GraphicObject>> _children = new();
+        public List<GraphicObject> Children => _children.Value;
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color}")
+                .AppendLine(Name);
+
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+
+    public class GeometricShapes
+    {
+        public static void Start(string[] args)
+        {
+            var drawing = new GraphicObject { Name = "My drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Square { Color = "Blue" });
+            group.Children.Add(new Circle { Color = "Blue" });
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
+        }
+    }
+}
---
+++ DesignPatterns.Composite/Examples/Neutral Networks.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Composite.Examples
+{
+    public static class NeuronExtensionsMethods
+    {
+        public static void Connect(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if(ReferenceEquals(self, other)) return;
+
+            foreach(Neuron from in self)
+            {
+                foreach(Neuron to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+            }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float value;
+        public List<Neuron> In = new(), Out = new();
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+
+    public class NeuronRing : List<Neuron>
+    {
+
+    }
+
+    public class Neutral_Networks
+    {
+        public static void Start(string[] args)
+        {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            neuron1.Connect(neuron2);
+
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            neuron1.Connect(layer1);
+            layer1.Connect(layer2);
+        }
+    }
+}
