f0ec438f68b774c886aaaa422dac5480885c594c
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-05 19:03:33 +0200

    Command: Command  & Undo Operation

---
+++ DesignPatterns.Command/Examples/Command.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+
+namespace DesignPatterns.Command.Examples
+{
+    public class BankAccount
+    {
+        private int balance;
+        private int overdraftLimit = -500;
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited: {amount} - balance: {balance}");
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (balance - amount >= overdraftLimit)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrew: {amount} - balance: {balance}");
+                return true;
+            }
+            return false;
+        }
+
+        public override string ToString() => $"{nameof(balance)}: {balance}";
+    }
+
+    public interface ICommand
+    {
+        void Call();
+        void Undo();
+    }
+
+    public class BankAccountCommand : ICommand
+    {
+        private BankAccount account;
+
+        public enum Action
+        {
+            Deposit, Withdraw
+        }
+
+        private Action action;
+        private int amount;
+        private bool succeeded;
+
+        public BankAccountCommand(BankAccount account, BankAccountCommand.Action action, int amount)
+        {
+            this.account = account;
+            this.action = action;
+            this.amount = amount;
+        }
+
+        public void Call()
+        {
+            switch (action)
+            {
+                case Action.Deposit:
+                    account.Deposit(amount);
+                    succeeded = true;
+                    break;
+                case Action.Withdraw:
+                    succeeded = account.Withdraw(amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Undo()
+        {
+            if (!succeeded) return;
+
+            switch (action)
+            {
+                case Action.Deposit:
+                    account.Withdraw(amount);
+                    break;
+                case Action.Withdraw:
+                    account.Deposit(amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class Command
+    {
+        public static void Start(string[] args)
+        {
+            var ba = new BankAccount();
+            var commands = new List<BankAccountCommand>
+            {
+                new BankAccountCommand(ba, BankAccountCommand.Action.Deposit, 100),
+                new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 1000),
+            };
+
+            Console.WriteLine(ba);
+
+            foreach(var c in commands)
+            {
+                c.Call();
+            }
+
+            foreach(var c in Enumerable.Reverse(commands))
+            {
+                c.Undo();
+            }
+
+            Console.WriteLine(ba);
+        }
+    }
+}
