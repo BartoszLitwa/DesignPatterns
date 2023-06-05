b59f3816016e7d11973b19b41f5c1329842c5ab7
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-05 18:39:45 +0200

    Chain Of Responsibility: Coding Exercise

--- DesignPatterns.ChainOfResponsibility/CodingExercise/ChainOfResponsibilityCodingExercise.cs
+++ DesignPatterns.ChainOfResponsibility/CodingExercise/ChainOfResponsibilityCodingExercise.cs
@@ -1,40 +1,66 @@
-﻿namespace DesignPatterns.ChainOfResponsibility.CodingExercise
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.ChainOfResponsibility.CodingExercise
 {
     public abstract class Creature
     {
-        protected Game _game;
-        protected int _attack, _defense;
+        public abstract int BaseAttack { get; }
+        public abstract int BaseDefense { get; }
 
-        public int Attack { get; set; }
-        public int Defense { get; set; }
+        public virtual int Attack { get; }
+        public virtual int Defense { get; }
+
+        protected Game game;
+
+        protected Creature(Game game)
+        {
+            this.game = game;
+        }
 
     }
 
     public class Goblin : Creature
     {
-        public Goblin(Game game) {
+        public override int BaseAttack => 1;
+        public override int BaseDefense => 1;
 
-            _game = game;
+        public Goblin(Game game) : base(game) { }
+
+        public override int Attack
+        {
+            get => BaseAttack + game.Creatures.OfType<GoblinKing>().Count();
         }
 
-        protected Goblin(Game game,  int attack = 1, int defense = 1) : this(game)
+        public override int Defense
         {
-            _attack = attack;
-            _defense = defense;
+            get => BaseDefense + game.Creatures.OfType<Goblin>().Count() - 1;
         }
     }
 
     public class GoblinKing : Goblin
     {
-        public GoblinKing(Game game) : base(game, 3, 3) { }
+        public override int BaseAttack => 3;
+        public override int BaseDefense => 3;
+
+        public GoblinKing(Game game) : base(game) { }
     }
 
     public class Game
     {
-        public IList<Creature> Creatures;
+        public IList<Creature> Creatures = new List<Creature>();
     }
 
     public class ChainOfResponsibilityCodingExercise
     {
+        public static void Start(string[] args)
+        {
+            var game = new Game();
+            var goblin = new Goblin(game);
+            game.Creatures.Add(goblin);
+            Console.WriteLine(goblin.Attack == 1);
+            Console.WriteLine(goblin.Defense == 1);
+        }
     }
 }
--- DesignPatterns.ChainOfResponsibility/Program.cs
+++ DesignPatterns.ChainOfResponsibility/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.ChainOfResponsibility.CodingExercise;
 using DesignPatterns.ChainOfResponsibility.Examples;
 using DesignPatterns.ChainOfResponsibility.Examples.BrokerChain;
 
 MethodChain.Start(args);
 
 BrokerChain.Start(args);
+
+ChainOfResponsibilityCodingExercise.Start(args);
