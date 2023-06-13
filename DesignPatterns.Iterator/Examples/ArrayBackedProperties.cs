c0bff4d1897ff128762fb3943514b3b8f4f37dac
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-13 22:38:25 +0200

    Iterator: Array Backed Properties

---
+++ DesignPatterns.Iterator/Examples/ArrayBackedProperties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Iterator.Examples.ArrayBackedProperties
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] _stats = new int[3];
+
+        private const int _strength = 0;
+        public int Strength { get => _stats[_strength]; set => _stats[_strength] = value; }
+
+        private const int _agility = 1;
+        public int Agility { get => _stats[_agility]; set => _stats[_agility] = value; }
+
+        private const int _intelligence = 2;
+        public int Intelligence { get => _stats[_intelligence]; set => _stats[_intelligence] = value; }
+
+        public double AvgStat => _stats.Average();
+        public double MinStat => _stats.Min();
+        public double MaxStat => _stats.Max();
+
+        public IEnumerator<int> GetEnumerator() => _stats.AsEnumerable().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public int this[int index]
+        {
+            get => _stats[index];
+            set => _stats[index] = value;
+        }
+    }
+
+    public class ArrayBackedProperties
+    {
+        public static void Start(string[] args)
+        {
+
+        }
+    }
+}
--- DesignPatterns.Iterator/Program.cs
+++ DesignPatterns.Iterator/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DesignPatterns.Iterator.Examples;
+using DesignPatterns.Iterator.Examples.ArrayBackedProperties;
 using DesignPatterns.Iterator.Examples.IteratorMethod;
 
 IteratorObject.Start(args);
 
 IteratorMethod.Start(args);
+
+ArrayBackedProperties.Start(args);
