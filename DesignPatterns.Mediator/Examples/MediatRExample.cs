f1e531be0ef9b51dac4255f948d98e6e05f54c88
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-15 21:44:34 +0200

    Mediator: MediatR

---
+++ DesignPatterns.Mediator/Examples/MediatRExample.cs
@@ -0,0 +1,61 @@
+﻿using Autofac;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Mediator.Examples
+{
+    public class PongResponse
+    {
+        public DateTime Timestamp;
+
+        public PongResponse(DateTime timestamp)
+        {
+            Timestamp = timestamp;
+        }
+    }
+
+    public class PingCommand : IRequest<PongResponse>
+    {
+
+    }
+
+    // [UsedImplicitly]
+    public class PingCommandHandler : IRequestHandler<PingCommand, PongResponse>
+    {
+        public async Task<PongResponse> Handle(PingCommand request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(new PongResponse(DateTime.Now))
+                .ConfigureAwait(false);
+        }
+    }
+
+    public class MediatRExample
+    {
+        public static async Task StartAsync(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(typeof(MediatRExample).Assembly)
+                .AsImplementedInterfaces();
+
+            builder.Register<Func<Type, object>>(context =>
+            {
+                var componentContext = context.Resolve<IComponentContext>();
+                return type => componentContext.Resolve(type);
+            });
+
+            builder.RegisterType<MediatR.Mediator>()
+                .As<IMediator>().InstancePerLifetimeScope();
+
+            using var container = builder.Build();
+            var mediator = container.Resolve<IMediator>();
+
+            var response = await mediator.Send(new PingCommand());
+            await Console.Out.WriteLineAsync($"We got a response at {response.Timestamp}");
+        }
+    }
+}
--- DesignPatterns.Mediator/Program.cs
+++ DesignPatterns.Mediator/Program.cs
@@ -5,3 +5,5 @@ using DesignPatterns.Mediator.Examples;
 ChatRoom.Start(args);
 
 EventBrokerExample.Start(args);
+
+await MediatRExample.StartAsync(args);
