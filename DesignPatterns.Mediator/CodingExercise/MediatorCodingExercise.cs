249217bedad0921b44ecca745fda984bcd98fa18
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-17 20:32:15 +0200

    Mediator: Coding Exercise

---
+++ DesignPatterns.Mediator/CodingExercise/MediatorCodingExercise.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Mediator.CodingExercise
+{
+    public class Participant
+    {
+        public int Value { get; set; }
+
+        private readonly Mediator _mediator;
+
+        public Participant(Mediator mediator)
+        {
+            _mediator = mediator;
+            _mediator.participants.Add(this);
+        }
+
+        public void Say(int n)
+        {
+            _mediator.Increment(n, this);
+        }
+    }
+
+    public class Mediator
+    {
+        public List<Participant> participants = new List<Participant>();
+
+        public Mediator()
+        {
+            
+        }
+
+        public void Increment(int n, Participant say)
+        {
+            foreach(var p in participants)
+                if(!p.Equals(say))
+                    p.Value += n;
+        }
+    }
+
+    public class MediatorCodingExercise
+    {
+        public static void Start(string[] args)
+        {
+
+        }
+    }
+}
--- DesignPatterns.Mediator/Examples/MediatRExample.cs
+++ DesignPatterns.Mediator/Examples/MediatRExample.cs
@@ -1,10 +1,5 @@
 ﻿using Autofac;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesignPatterns.Mediator.Examples
 {
