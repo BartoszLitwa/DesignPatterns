47b6c5511db9be825bb9918aac870abb3e878a5d
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-18 15:34:59 +0200

    NullObject: Coding Exercise

---
+++ DesignPatterns.NullObject/CodingExercise/NullObjectCodingExercise.cs
@@ -0,0 +1,45 @@
+﻿namespace DesignPatterns.NullObject.CodingExercise
+{
+    public interface ILog
+    {
+        // maximum # of elements in the log
+        int RecordLimit { get; }
+
+        // number of elements already in the log
+        int RecordCount { get; set; }
+
+        // expected to increment RecordCount
+        void LogInfo(string message);
+    }
+
+    public class Account
+    {
+        private ILog log;
+
+        public Account(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void SomeOperation()
+        {
+            int c = log.RecordCount;
+            log.LogInfo("Performing an operation");
+            if (c + 1 != log.RecordCount)
+                throw new Exception();
+            if (log.RecordCount >= log.RecordLimit)
+                throw new Exception();
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        public int RecordLimit { get; } = int.MaxValue;
+        public int RecordCount { get; set; } = int.MinValue;
+        public void LogInfo(string message) => ++RecordCount;
+    }
+
+    public class NullObjectCodingExercise
+    {
+    }
+}
