1ac023e6f9a7d2b729dce4d4920531ed8b3ad9c7
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-20 19:51:52 +0200

    Observer: Weak Event Pattern

---
+++ DesignPatterns.Observer/Examples/WeakEventPattern.cs
@@ -0,0 +1,58 @@
+﻿namespace DesignPatterns.Observer.Examples
+{
+    public class Button
+    {
+        public event EventHandler Clicked;
+
+        public void Fire()
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Window
+    {
+        public Window(Button button)
+        {
+            button.Clicked += ButtonOnClicked;
+            // .NET Framework
+            //WeakEventManager<Button, EventArgs>
+            //    .AddHandler(button, "Clicked", ButtonOnClicked);
+        }
+        private void ButtonOnClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("Button Clicked (window handler)");
+        }
+
+        ~Window()
+        {
+            Console.WriteLine("Window Destructor");
+        }
+    }
+
+    public class WeakEventPattern
+    {
+        public static void Start(string[] args)
+        {
+            var btn = new Button();
+            var window = new Window(btn);
+            var windowRef = new WeakReference(window);
+            btn.Fire();
+
+            Console.WriteLine("Setting window to null");
+            window = null;
+
+            FireGC();
+            Console.WriteLine($"Is window Alive: {windowRef.IsAlive}");
+        }
+
+        private static void FireGC()
+        {
+            Console.WriteLine("Starting GC");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Console.WriteLine("Ending GC");
+        }
+    }
+}
--- DesignPatterns.Observer/Program.cs
+++ DesignPatterns.Observer/Program.cs
@@ -1,3 +1,6 @@
-﻿using DesignPatterns.Observer.Examples.ObserverViaTheEventKeyword;
+﻿using DesignPatterns.Observer.Examples;
+using DesignPatterns.Observer.Examples.ObserverViaTheEventKeyword;
 
 ObserverViaTheEventKeyword.Start(args);
+
+WeakEventPattern.Start(args);
