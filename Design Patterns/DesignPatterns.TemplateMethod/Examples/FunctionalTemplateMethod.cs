34ee522e77e564c72a0ac2773504cf02e657c384
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-22 20:29:41 +0200

    TemplateMethod: Function Template Method & Coding Exercise

---
+++ Design Patterns/DesignPatterns.TemplateMethod/Examples/FunctionalTemplateMethod.cs
@@ -0,0 +1,41 @@
+﻿namespace DesignPatterns.TemplateMethod.Examples.FunctionalTemplateMethod
+{
+    public static class GameTemplate
+    {
+        public static void Run(
+            Action start,
+            Action takeTurn,
+            Func<bool> haveWinner,
+            Func<int> winningPlayer)
+        {
+            start();
+            while (!haveWinner())
+                takeTurn();
+            Console.WriteLine($"Player {winningPlayer()} wins.");
+        }
+    }
+
+    public class FunctionalTemplateMethod
+    {
+        public static void Start(string[] args)
+        {
+            var numberOfPlayers = 2;
+            int currentPlayer = 0;
+            int turn = 1, maxTurns = 10;
+
+            void Start() => Console.WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+
+            bool HaveWinner() => turn == maxTurns;
+
+            void TakeTurn()
+            {
+                Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}");
+                currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+            }
+
+            int WinningPlayer() => currentPlayer;
+
+            GameTemplate.Run(Start, TakeTurn, HaveWinner, WinningPlayer);
+        }
+    }
+}
--- Design Patterns/DesignPatterns.TemplateMethod/Program.cs
+++ Design Patterns/DesignPatterns.TemplateMethod/Program.cs
@@ -1,3 +1,6 @@
-﻿using DesignPatterns.TemplateMethod.Examples.TemplateMethod;
+﻿using DesignPatterns.TemplateMethod.Examples.FunctionalTemplateMethod;
+using DesignPatterns.TemplateMethod.Examples.TemplateMethod;
 
 TemplateMethod.Start(args);
+
+FunctionalTemplateMethod.Start(args);
