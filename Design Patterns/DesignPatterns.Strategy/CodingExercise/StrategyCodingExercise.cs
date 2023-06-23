eda1d9e73ff9560cc652525327452b81a5ee343b
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-23 22:53:08 +0200

    Strategy: Equality and Comparision Strategies

---
+++ Design Patterns/DesignPatterns.Strategy/CodingExercise/StrategyCodingExercise.cs
@@ -0,0 +1,52 @@
+﻿using System.Numerics;
+
+namespace DesignPatterns.Strategy.CodingExercise.StrategyCodingExercise
+{
+    public interface IDiscriminantStrategy
+    {
+        double CalculateDiscriminant(double a, double b, double c);
+    }
+
+    public class OrdinaryDiscriminantStrategy : IDiscriminantStrategy
+    {
+        // todo
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            var discriminant = b * b - 4 * a * c;
+            return discriminant;
+        }
+    }
+
+    public class RealDiscriminantStrategy : IDiscriminantStrategy
+    {
+        // todo (return NaN on negative discriminant!)
+        public double CalculateDiscriminant(double a, double b, double c)
+        {
+            var discriminant = b * b - 4 * a * c;
+            return discriminant < 0 ? double.NaN : discriminant;
+        }
+    }
+
+    public class QuadraticEquationSolver
+    {
+        private readonly IDiscriminantStrategy strategy;
+
+        public QuadraticEquationSolver(IDiscriminantStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public Tuple<Complex, Complex> Solve(double a, double b, double c)
+        {
+            var disc = new Complex(strategy.CalculateDiscriminant(a, b, c), 0);
+            var rootDisc = Complex.Sqrt(disc);
+            var plusX = (-b + rootDisc) / (2 * a);
+            var minusX = (-b - rootDisc) / (2 * a);
+            return new Tuple<Complex, Complex>(minusX, plusX);
+        }
+    }
+
+    public class StrategyCodingExercise
+    {
+    }
+}
--- Design Patterns/DesignPatterns.Strategy/Program.cs
+++ Design Patterns/DesignPatterns.Strategy/Program.cs
@@ -1,6 +1,9 @@
 ﻿using DesignPatterns.Strategy.Examples.DynamicStrategy;
+using DesignPatterns.Strategy.Examples.EqualityAndComparisionStrategies;
 using DesignPatterns.Strategy.Examples.StaticStrategy;
 
 DynamicStrategy.Start(args);
 
 StaticStrategy.Start(args);
+
+EqualityAndComparisionStrategies.Start(args);
