d439150b9e47b2cd2e696049daf96363155f6188
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-24 22:31:46 +0200

    State: Switch Based State Machine

---
+++ Design Patterns/DesignPatterns.State/Examples/SwitchBasedStateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace DesignPatterns.State.Examples.SwitchBasedStateMachine
+{
+    public enum State
+    {
+        Locked, Failed, Unlocked
+    }
+
+    public class SwitchBasedStateMachine
+    {
+        public static void Start(string[] args)
+        {
+            var code = "1234";
+            var state = State.Locked;
+            var entry = new StringBuilder();
+
+            while (true)
+            {
+                switch(state)
+                {
+                    case State.Locked:
+                        entry.Append(Console.ReadKey().KeyChar);
+                        if(entry.ToString() == code)
+                        {
+                            state = State.Unlocked;
+                            break;
+                        }
+
+                        if (!code.StartsWith(entry.ToString()))
+                        {
+                            //state = State.Failed;
+                            goto case State.Failed;
+                        }
+
+                        break;
+                    case State.Failed:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("Failed");
+                        entry.Clear();
+                        state = State.Locked;
+                        break;
+                    case State.Unlocked:
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("Unlocked");
+                        return;
+                }
+            }
+        }
+    }
+}
--- Design Patterns/DesignPatterns.State/Program.cs
+++ Design Patterns/DesignPatterns.State/Program.cs
@@ -1,6 +1,9 @@
 ﻿using DesignPatterns.State.Examples.ClassicImplementation;
 using DesignPatterns.State.Examples.HandmadeStateMachine;
+using DesignPatterns.State.Examples.SwitchBasedStateMachine;
 
 ClassicImplementation.Start(args);
 
 HandmadeStateMachine.Start(args);
+
+SwitchBasedStateMachine.Start(args);
