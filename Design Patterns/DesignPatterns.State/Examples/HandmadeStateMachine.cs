c22ff5d55fc4d61b7a73dd7372d810ef124b4c79
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-24 22:24:40 +0200

    State: Handmade State Machine

---
+++ Design Patterns/DesignPatterns.State/Examples/HandmadeStateMachine.cs
@@ -0,0 +1,70 @@
+﻿namespace DesignPatterns.State.Examples.HandmadeStateMachine
+{
+    public enum State
+    {
+        OffHook,
+        Connecting,
+        Connected,
+        OnHold
+    }
+
+    public enum Trigger
+    {
+        CallDialed,
+        HungUp,
+        CallConnected,
+        PlacedOnHold,
+        TakenOffHold,
+        LeftMessage
+    }
+
+    public class HandmadeStateMachine
+    {
+        private static Dictionary<State, List<(Trigger, State)>> rules = new()
+        {
+            [State.OffHook] = new List<(Trigger, State)>
+            {
+              (Trigger.CallDialed, State.Connecting)
+            },
+            [State.Connecting] = new List<(Trigger, State)>
+            {
+              (Trigger.HungUp, State.OffHook),
+              (Trigger.CallConnected, State.Connected)
+            },
+            [State.Connected] = new List<(Trigger, State)>
+            {
+              (Trigger.LeftMessage, State.OffHook),
+              (Trigger.HungUp, State.OffHook),
+              (Trigger.PlacedOnHold, State.OnHold)
+            },
+            [State.OnHold] = new List<(Trigger, State)>
+            {
+              (Trigger.TakenOffHold, State.Connected),
+              (Trigger.HungUp, State.OffHook)
+            }
+        };
+
+        public static void Start(string[] args)
+        {
+            var state = State.OffHook;
+            while (true)
+            {
+                Console.WriteLine($"The phone is currently {state}");
+                Console.WriteLine("Select a trigger: ");
+
+                for (int i = 0; i < rules[state].Count; i++)
+                {
+                    var (trigger, _) = rules[state][i];
+                    Console.WriteLine($"{i}. {trigger}");
+                }
+
+                //var input = int.Parse(Console.ReadLine());
+                var input = 0;
+
+                var (_, s) = rules[state][input];
+                state = s;
+                break;
+            }
+        }
+    }
+}
--- Design Patterns/DesignPatterns.State/Program.cs
+++ Design Patterns/DesignPatterns.State/Program.cs
@@ -1,3 +1,6 @@
 ﻿using DesignPatterns.State.Examples.ClassicImplementation;
+using DesignPatterns.State.Examples.HandmadeStateMachine;
 
 ClassicImplementation.Start(args);
+
+HandmadeStateMachine.Start(args);
