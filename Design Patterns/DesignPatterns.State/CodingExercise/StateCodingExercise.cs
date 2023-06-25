bdb533bd4a0056e6d754c0120058fe66a3737927
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-25 22:58:52 +0200

    State: Coding Exercise

---
+++ Design Patterns/DesignPatterns.State/CodingExercise/StateCodingExercise.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace DesignPatterns.State.CodingExercise
+{
+    public enum LockStates
+    {
+        LOCKED, OPEN, ERROR
+    }
+
+    public class CombinationLock
+    {
+        private readonly string _combination;
+        public CombinationLock(int[] combination)
+        {
+            var sb = new StringBuilder();
+            foreach (int i in combination)
+                sb.Append(i);
+
+            _combination = sb.ToString();
+        }
+
+        // you need to be changing this on user input
+        public string Status = "LOCKED";
+        private LockStates state = LockStates.LOCKED;
+
+        public void EnterDigit(int digit)
+        {
+            if (Status == "LOCKED")
+                Status = "";
+
+            switch(state)
+            {
+                case LockStates.LOCKED:
+                    Status += digit;
+                    if(Status == _combination)
+                    {
+                        Status = "OPEN";
+                        state = LockStates.OPEN;
+                    } else if(Status.Length >= _combination.Length)
+                    {
+                        Status = "ERROR";
+                        state = LockStates.ERROR;
+                    }
+
+                    break;
+                case LockStates.OPEN:
+
+                    break;
+                case LockStates.ERROR:
+
+                    break;
+            }
+        }
+    }
+
+    public class StateCodingExercise
+    {
+        public static void Start(string[] args)
+        {
+
+        }
+    }
+}
