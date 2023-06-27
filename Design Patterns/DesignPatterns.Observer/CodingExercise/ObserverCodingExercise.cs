29dba52d11fcecffde6c6815b9b498972edf88c8
BartoszLitwa <bartosz.litwa@proton.me>
2023-06-27 22:00:17 +0200

    Observer: Observer Coding Exercise

---
+++ Design Patterns/DesignPatterns.Observer/CodingExercise/ObserverCodingExercise.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+
+namespace DesignPatterns.Observer.CodingExercise.ObserverCodingExercise
+{
+    public class Game
+    {
+        public event EventHandler RatEnters, RatDies;
+        public event EventHandler<Rat> NotifyRat;
+
+        public void FireRatEnters(object sender) => RatEnters?.Invoke(sender, EventArgs.Empty);
+
+        public void FireRatDies(object sender) => RatDies?.Invoke(sender, EventArgs.Empty);
+
+        public void FireNotifyRat(object sender, Rat whichRat) => NotifyRat?.Invoke(sender, whichRat);
+    }
+
+    public class Rat : IDisposable
+    {
+        public int Attack = 1;
+
+        private readonly Game _game;
+
+        public Rat(Game game)
+        {
+            _game = game;
+            _game.RatEnters += (sender, args) =>
+            {
+                if (sender != this)
+                {
+                    ++Attack;
+                    game.FireNotifyRat(this, (Rat)sender);
+                }
+            };
+            _game.NotifyRat += (sender, rat) =>
+            {
+                if (rat == this) ++Attack;
+            };
+            _game.RatDies += (sender, args) => --Attack;
+            _game.FireRatEnters(this);
+        }
+
+
+        public void Dispose() => _game.FireRatDies(this);
+    }
+
+    public class ObserverCodingExercise
+    {
+        public static void Start(string[] args)
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void PlayingByTheRules()
+        {
+            Assert.That(typeof(Game).GetFields(), Is.Empty);
+            Assert.That(typeof(Game).GetProperties(), Is.Empty);
+        }
+
+        [Test]
+        public void SingleRatTest()
+        {
+            var game = new Game();
+            var rat = new Rat(game);
+            Assert.That(rat.Attack, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TwoRatTest()
+        {
+            var game = new Game();
+            var rat = new Rat(game);
+            var rat2 = new Rat(game);
+            Assert.That(rat.Attack, Is.EqualTo(2));
+            Assert.That(rat2.Attack, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ThreeRatsOneDies()
+        {
+            var game = new Game();
+
+            var rat = new Rat(game);
+            Assert.That(rat.Attack, Is.EqualTo(1));
+
+            var rat2 = new Rat(game);
+            Assert.That(rat.Attack, Is.EqualTo(2));
+            Assert.That(rat2.Attack, Is.EqualTo(2));
+
+            using (var rat3 = new Rat(game))
+            {
+                Assert.That(rat.Attack, Is.EqualTo(3));
+                Assert.That(rat2.Attack, Is.EqualTo(3));
+                Assert.That(rat3.Attack, Is.EqualTo(3));
+            }
+
+            Assert.That(rat.Attack, Is.EqualTo(2));
+            Assert.That(rat2.Attack, Is.EqualTo(2));
+        }
+    }
+}
--- Design Patterns/DesignPatterns.Observer/Examples/DeclarativeEventSubscriptionsWithInterfaces.cs
+++ Design Patterns/DesignPatterns.Observer/Examples/DeclarativeEventSubscriptionsWithInterfaces.cs
@@ -36,8 +36,13 @@ namespace DesignPatterns.Observer.ExamplesDeclarativeEventSubscriptionsWithInter
         }
     }
 
-    public class Logging : IHandle<ButtonPressedEvent>
+    public class Logging : IHandle<ButtonPressedEvent>, IDisposable
     {
+        public void Dispose()
+        {
+
+        }
+
         public void Handle(object sender, ButtonPressedEvent args)
         {
             Console.WriteLine($"Button clicked {args.NumberOfClicks} times");
