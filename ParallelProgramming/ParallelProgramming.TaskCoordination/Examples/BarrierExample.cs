89beecd931dc68249bdd2679ff9633e7eda54190
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-01 20:56:11 +0200

    Task Coordination: Barrier

--- ParallelProgramming/ParallelProgramming.TaskCoordination/Examples/BarrierExample.cs
+++ ParallelProgramming/ParallelProgramming.TaskCoordination/Examples/BarrierExample.cs
@@ -2,9 +2,41 @@
 {
     public class BarrierExample
     {
+        static Barrier barrier = new Barrier(2, b =>
+        {
+            Console.WriteLine($"Phase {b.CurrentPhaseNumber} is finished");
+        });
+
+        public static void Water()
+        {
+            Console.WriteLine("Putting the kettle on (takes a bit longer)");
+            Thread.Sleep(1000);
+            // signal -> wait
+            barrier.SignalAndWait(); // 2
+            Console.WriteLine("Pouring water into cup."); // 0
+            barrier.SignalAndWait(); // 1
+            Console.WriteLine("Putting the kettle away");
+        }
+
+        public static void Cup()
+        {
+            Console.WriteLine("Finding the nicest a cup of tea (fast)");
+            barrier.SignalAndWait(); // 1
+            Console.WriteLine("Adding tea."); // 0
+            barrier.SignalAndWait(); // 2
+            Console.WriteLine("Adding sugar");
+        }
+
         public static void Start(string[] args)
         {
+            var water = Task.Factory.StartNew(Water);
+            var cup = Task.Factory.StartNew(Cup);
 
+            var tea = Task.Factory.ContinueWhenAll(new[] { water, cup }, tasks =>
+            {
+                Console.WriteLine("Enjoy your cup of tea.");
+            });
+            tea.Wait();
         }
     }
 }
--- ParallelProgramming/ParallelProgramming.TaskCoordination/Program.cs
+++ ParallelProgramming/ParallelProgramming.TaskCoordination/Program.cs
@@ -2,11 +2,11 @@
 using ParallelProgramming.TaskCoordination.Examples.ChildTasks;
 using ParallelProgramming.TaskCoordination.Examples.Continuations;
 
-Console.WriteLine($"{nameof(Continuations)}\n");
+Console.WriteLine($"\n{nameof(Continuations)}\n");
 Continuations.Start(args);
 
-Console.WriteLine($"{nameof(ChildTasks)}\n");
+Console.WriteLine($"\n{nameof(ChildTasks)}\n");
 ChildTasks.Start(args);
 
-Console.WriteLine($"{nameof(BarrierExample)}\n");
+Console.WriteLine($"\n{nameof(BarrierExample)}\n");
 BarrierExample.Start(args);
