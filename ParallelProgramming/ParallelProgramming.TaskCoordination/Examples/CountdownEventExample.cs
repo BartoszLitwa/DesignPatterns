aef6f492e4741446892c483c00286d61ce45366f
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-01 21:10:04 +0200

    Task Coordination: Countdown Event

---
+++ ParallelProgramming/ParallelProgramming.TaskCoordination/Examples/CountdownEventExample.cs
@@ -0,0 +1,31 @@
+﻿namespace ParallelProgramming.TaskCoordination.Examples.CountdownEventExample
+{
+    public class CountdownEventExample
+    {
+        private static int taskCount = 5;
+        static CountdownEvent cte = new CountdownEvent(taskCount);
+        private static Random random = new Random();
+
+        public static void Start(string[] args)
+        {
+            for(int i = 0; i < taskCount; i++)
+            {
+                var task = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"Entering task {Task.CurrentId}");
+                    Thread.Sleep(random.Next(300));
+                    cte.Signal();
+                    Console.WriteLine($"Exiting task {Task.CurrentId}");
+                });
+            }
+
+            var finalTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Waiting for other tasks to complete in {Task.CurrentId}");
+                cte.Wait(); // Blocking invokation
+                Console.WriteLine("All tasks completed");
+            });
+            finalTask.Wait();
+        }
+    }
+}
--- ParallelProgramming/ParallelProgramming.TaskCoordination/Program.cs
+++ ParallelProgramming/ParallelProgramming.TaskCoordination/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ParallelProgramming.TaskCoordination.Examples.BarrierExample;
 using ParallelProgramming.TaskCoordination.Examples.ChildTasks;
 using ParallelProgramming.TaskCoordination.Examples.Continuations;
+using ParallelProgramming.TaskCoordination.Examples.CountdownEventExample;
 
 Console.WriteLine($"\n{nameof(Continuations)}\n");
 Continuations.Start(args);
@@ -10,3 +11,6 @@ ChildTasks.Start(args);
 
 Console.WriteLine($"\n{nameof(BarrierExample)}\n");
 BarrierExample.Start(args);
+
+Console.WriteLine($"\n{nameof(CountdownEventExample)}\n");
+CountdownEventExample.Start(args);
