a53bcd8478e9bdeff35305fc3a583b03992e9f9a
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-04 17:55:40 +0200

    Parallel Loops: Parallel Invoke/For/ForEach
       This new project includes the addition of a new file "ParallelInvokeForForEach.cs", which showcases the usage of multi-threading features like parallel Invoke, For, and ForEach in .NET. This is done by printing the length of words and squares of numbers concurrently, and also executing three different methods in parallel. This will be beneficial for users who wants to understand and implement parallel processing in their .NET solutions.

---
+++ ParallelProgramming/ParallelProgramming.ParallelLoops/Examples/ParallelInvokeForForEach.cs
@@ -0,0 +1,41 @@
+﻿namespace ParallelProgramming.ParallelLoops.Examples.ParallelInvokeForForEach;
+
+public class ParallelInvokeForForEach
+{
+    public static IEnumerable<int> CustomRange(int start, int end, int step)
+    {
+        for (int i = start; i < end; i += step)
+        {
+            yield return i;
+        }
+    }
+    
+    public static void Start(string[] args)
+    {
+        Invoke();
+        For();
+        ForEach();
+
+        var po = new ParallelOptions();
+        Parallel.ForEach(CustomRange(1, 20, 3), po, Console.WriteLine);
+    }
+
+    private static void ForEach()
+    {
+        string[] words = { "oh", "what", "a", "day" };
+        Parallel.ForEach(words, word => { Console.WriteLine($"{word} has length {word.Length} - {Task.CurrentId}"); });
+    }
+
+    private static void For()
+    {
+        Parallel.For(1, 11, i => { Console.WriteLine($"{i * i}\t"); });
+    }
+
+    private static void Invoke()
+    {
+        var a = new Action(() => Console.WriteLine($"First {Task.CurrentId}"));
+        var b = new Action(() => Console.WriteLine($"Second {Task.CurrentId}"));
+        var c = new Action(() => Console.WriteLine($"Third {Task.CurrentId}"));
+        Parallel.Invoke(a, b, c);
+    }
+}
