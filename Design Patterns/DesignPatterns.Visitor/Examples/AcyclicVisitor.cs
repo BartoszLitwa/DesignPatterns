c3734b22b6547c82f960267f943b23b5931c5f01
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-04 17:15:41 +0200

    Visitor: Acyclic Visitor
    The Acyclic Visitor design pattern was implemented to handle operations on different types of expressions, namely DoubleExpression and AdditionExpression. This pattern eliminates the need to modify existing code when adding new types, hence improving the system's extensibility. New visitor interfaces can now be easily added to handle more expression types in the future. The main program was also updated to call the new AcyclicVisitor.

---
+++ Design Patterns/DesignPatterns.Visitor/Examples/AcyclicVisitor.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+
+namespace DesignPatterns.Visitor.Examples.AcyclicVisitor;
+
+public interface IVisitor<TVisitable>
+{
+    void Visit(TVisitable obj);
+}
+
+public interface IVisitor { }
+
+// 3 - DoubleExpression
+// (1+2) (1+(2+3)) Addition Expression
+public abstract class Expression
+{
+    public virtual void Accept(IVisitor visitor)
+    {
+        if (visitor is IVisitor<Expression> typed)
+            typed.Visit(this);
+    }
+}
+
+public class DoubleExpression : Expression
+{
+    public double Value;
+
+    public DoubleExpression(double value)
+    {
+        Value = value;
+    }
+
+    public override void Accept(IVisitor visitor)
+    {
+        if (visitor is IVisitor<DoubleExpression> typed)
+            typed.Visit(this);
+    }
+}
+
+public class AdditionExpression : Expression
+{
+    public Expression Left, Right;
+
+    public AdditionExpression(Expression left, Expression right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public override void Accept(IVisitor visitor)
+    {
+        if (visitor is IVisitor<AdditionExpression> typed)
+            typed.Visit(this);
+    }
+}
+
+public class ExpressionPrinter : IVisitor,
+    IVisitor<Expression>, IVisitor<DoubleExpression>, IVisitor<AdditionExpression>
+{
+    private StringBuilder sb = new StringBuilder();
+    
+    public void Visit(Expression obj)
+    {
+    }
+
+    public void Visit(DoubleExpression obj)
+    {
+        
+    }
+
+    public void Visit(AdditionExpression obj)
+    {
+        sb.Append('(');
+        obj.Left.Accept(this);
+        sb.Append('+');
+        obj.Right.Accept(this);
+        sb.Append(')');
+    }
+
+    public override string ToString() => sb.ToString();
+}
+
+public class AcyclicVisitor
+{
+    public static void Start(string[] args)
+    {
+        var expr = new AdditionExpression(
+            new DoubleExpression(1),
+            new AdditionExpression(
+                new DoubleExpression(2),
+                new DoubleExpression(3)
+            ));
+
+        var ep = new ExpressionPrinter();
+        ep.Visit(expr);
+        Console.WriteLine(ep);
+    }
+}
--- Design Patterns/DesignPatterns.Visitor/Program.cs
+++ Design Patterns/DesignPatterns.Visitor/Program.cs
@@ -1,4 +1,5 @@
-﻿using DesignPatterns.Visitor.Examples.ClassicVisitorDoubleDispatch;
+﻿using DesignPatterns.Visitor.Examples.AcyclicVisitor;
+using DesignPatterns.Visitor.Examples.ClassicVisitorDoubleDispatch;
 using DesignPatterns.Visitor.Examples.DynamicVisitorViaTheDLR;
 using DesignPatterns.Visitor.Examples.IntrusiveVisitor;
 using DesignPatterns.Visitor.Examples.ReductionsAndTransforms;
@@ -18,3 +19,6 @@ ReductionsAndTransforms.Start(args);
 
 Console.WriteLine($"\n{nameof(DynamicVisitorViaTheDLR)}\n");
 DynamicVisitorViaTheDLR.Start(args);
+
+Console.WriteLine($"\n{nameof(AcyclicVisitor)}\n");
+AcyclicVisitor.Start(args);
