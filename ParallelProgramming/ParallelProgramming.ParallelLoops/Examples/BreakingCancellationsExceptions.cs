f670b15897fc24b2e5b996ca2c1ce5c8a0d99e56
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-06 17:39:32 +0200

    Parallel loops: Breaking, Cancellation and Exceptions
    This commit brings a new practical scenario showing how to handle break, cancel and exceptions during a parallel loop execution. This aims to showcase best practices and potential pitfalls when using parallelization tools. A proper use of these methods can help enhance performance and handle complexity in a more efficient way when dealing with concurrent tasks.

---
+++ ParallelProgramming/ParallelProgramming.ParallelLoops/Examples/BreakingCancellationsExceptions.cs
@@ -0,0 +1,52 @@
+﻿namespace ParallelProgramming.ParallelLoops.Examples.BreakingCancellationsExceptions;
+
+public class BreakingCancellationsExceptions
+{
+    public static void Start(string[] args)
+    {
+        try
+        {
+            Demo();
+
+        }
+        catch (AggregateException ae)
+        {
+            ae.Handle(e =>
+            {
+                Console.WriteLine(e.Message);
+                return true;
+            });
+        }
+        catch (OperationCanceledException oce)
+        {
+            Console.WriteLine(oce.Message);
+        }
+
+    }
+
+    private static void Demo()
+    {
+        var cts = new CancellationTokenSource();
+        var po = new ParallelOptions()
+        {
+            CancellationToken = cts.Token
+        };
+        var result = Parallel.For(0, 20, po, (int i, ParallelLoopState state) =>
+        {
+            Console.WriteLine($"{i} - [{Task.CurrentId}]");
+
+            if (i == 10)
+            {
+                // state.Stop();
+                // state.Break();
+                // throw new Exception();
+                
+                cts.Cancel();
+            }
+        });
+        
+        Console.WriteLine($"\n Was loop completed? {result.IsCompleted}");
+        if(result.LowestBreakIteration.HasValue)
+            Console.WriteLine($"Lowest break iteration is {result.LowestBreakIteration}");
+    }
+}
--- ParallelProgramming/ParallelProgramming.ParallelLoops/Program.cs
+++ ParallelProgramming/ParallelProgramming.ParallelLoops/Program.cs
@@ -1,3 +1,8 @@
-﻿using ParallelProgramming.ParallelLoops.Examples.ParallelInvokeForForEach;
+﻿using ParallelProgramming.ParallelLoops.Examples.BreakingCancellationsExceptions;
+using ParallelProgramming.ParallelLoops.Examples.ParallelInvokeForForEach;
 
+Console.WriteLine($"\n{nameof(ParallelInvokeForForEach)}\n");
 ParallelInvokeForForEach.Start(args);
+
+Console.WriteLine($"\n{nameof(BreakingCancellationsExceptions)}\n");
+BreakingCancellationsExceptions.Start(args);
