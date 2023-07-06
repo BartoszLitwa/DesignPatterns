943ba33844172a4051efc989524721a9a5c415c6
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-06 17:47:01 +0200

    Parallel loops: Thread Local Storage
    This new class showcases thread-safe sum calculation in a parallel for loop using thread local storage and Interlocked method. It performs sum of numbers from 1 to 1000 in a safe manner when multiple threads are involved.

---
+++ ParallelProgramming/ParallelProgramming.ParallelLoops/Examples/ThreadLocalStorage.cs
@@ -0,0 +1,33 @@
+﻿namespace ParallelProgramming.ParallelLoops.Examples.ThreadLocalStorage;
+
+public class ThreadLocalStorage
+{
+    public static void Start(string[] args)
+    {
+        InterlockedVersion();
+
+        int sum = 0;
+
+        Parallel.For(1, 1001,
+            () => 0, // Thread local storage initialization
+            (x, state, tls) =>
+            {
+                tls += x;
+                Console.WriteLine($"Task {Task.CurrentId} has sum {tls}");
+                return tls;
+            },
+            parttalSum => // Sum the calculated partial values
+            {
+                Console.WriteLine($"Partial value of task {Task.CurrentId} is {parttalSum}");
+                Interlocked.Add(ref sum, parttalSum);
+            });
+
+        Console.WriteLine($"Sum of 1..1000 = {sum}");
+    }
+
+    private static void InterlockedVersion()
+    {
+        int sum = 0;
+        Parallel.For(1, 1001, x => { Interlocked.Add(ref sum, x); });
+    }
+}
--- ParallelProgramming/ParallelProgramming.ParallelLoops/Program.cs
+++ ParallelProgramming/ParallelProgramming.ParallelLoops/Program.cs
@@ -1,8 +1,12 @@
 ﻿using ParallelProgramming.ParallelLoops.Examples.BreakingCancellationsExceptions;
 using ParallelProgramming.ParallelLoops.Examples.ParallelInvokeForForEach;
+using ParallelProgramming.ParallelLoops.Examples.ThreadLocalStorage;
 
 Console.WriteLine($"\n{nameof(ParallelInvokeForForEach)}\n");
 ParallelInvokeForForEach.Start(args);
 
 Console.WriteLine($"\n{nameof(BreakingCancellationsExceptions)}\n");
 BreakingCancellationsExceptions.Start(args);
+
+Console.WriteLine($"\n{nameof(ThreadLocalStorage)}\n");
+ThreadLocalStorage.Start(args);
