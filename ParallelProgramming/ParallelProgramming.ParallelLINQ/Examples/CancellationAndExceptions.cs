58cd059d93821917553d87d49d82a6dd7051b9e2
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-08 16:42:54 +0200

    Parallel LINQ: Cancellation and Exceptions Introduced a new example class "CancellationAndExceptions" within the ParallelProgramming.ParallelLINQ module. This class demonstrates how to handle cancellations and exceptions in parallel LINQ. Updated the main program to also call and execute this newly added example for more comprehensive demonstration of parallel LINQ's capabilities.

---
+++ ParallelProgramming/ParallelProgramming.ParallelLINQ/Examples/CancellationAndExceptions.cs
@@ -0,0 +1,43 @@
+﻿namespace ParallelProgramming.ParallelLINQ.Examples.CancellationAndExceptions;
+
+public class CancellationAndExceptions
+{
+    public static void Start(string[] args)
+    {
+        var cts = new CancellationTokenSource();
+        
+        var items = ParallelEnumerable.Range(1, 20);
+        var results = items.WithCancellation(cts.Token)
+            .Select(i =>
+            {
+                var result = Math.Log10(i);
+                if (result > 1)
+                    throw new InvalidOperationException();
+                
+                Console.WriteLine($"i = {i}, tid = {Task.CurrentId}");
+                return result;
+            });
+
+        try
+        {
+            foreach (var c in results)
+            {
+                if (c > 1)
+                    cts.Cancel();
+                Console.WriteLine($"results = {c}");
+            }
+        }
+        catch (AggregateException ae)
+        {
+            ae.Handle(e =>
+            {
+                Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+                return true;
+            });
+        }
+        catch (OperationCanceledException oce)
+        {
+            Console.WriteLine("Canceled");
+        }
+    }
+}
--- ParallelProgramming/ParallelProgramming.ParallelLINQ/Program.cs
+++ ParallelProgramming/ParallelProgramming.ParallelLINQ/Program.cs
@@ -1,4 +1,8 @@
 ﻿using ParallelProgramming.ParallelLINQ.Examples.AsParallelAndParallelQuery;
+using ParallelProgramming.ParallelLINQ.Examples.CancellationAndExceptions;
 
 Console.WriteLine($"\n{nameof(AsParallelAndParallelQuery)}\n");
 AsParallelAndParallelQuery.Start(args);
+
+Console.WriteLine($"\n{nameof(CancellationAndExceptions)}\n");
+CancellationAndExceptions.Start(args);
