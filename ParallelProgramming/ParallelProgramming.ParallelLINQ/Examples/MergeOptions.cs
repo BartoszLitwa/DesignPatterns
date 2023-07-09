c3dd2de514eac84d54551cfc5c6768a2ad2f59b2
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-09 22:21:06 +0200

    Parallel LINQ: Merge Options Added new example for merge options in Parallel LINQ.
    MergeOptions class introduced to demonstrate the usage of MergeOptions in Parallel LINQ. This new file includes a method to perform concurrent calculations on a range of numbers using different MergeOptions settings. This addition allows detailed inspection of performance differences between different MergeOptions in a parallel queries environment. Also updated the main program to call this new example.

---
+++ ParallelProgramming/ParallelProgramming.ParallelLINQ/Examples/MergeOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace ParallelProgramming.ParallelLINQ.Examples.MergeOptions;
+
+public class MergeOptions
+{
+    public static void Start(string[] args)
+    {
+        var numbers = Enumerable.Range(1, 20).ToArray();
+
+        var results = numbers.AsParallel()
+            .WithMergeOptions(ParallelMergeOptions.NotBuffered)
+            // .WithMergeOptions(ParallelMergeOptions.FullyBuffered)
+            .Select(x =>
+            {
+                var result = Math.Log10(x);
+                Console.WriteLine($"Produced {result}");
+                return result;
+            });
+
+        foreach (var i in results)
+        {
+            Console.WriteLine($"Consumed {i}");
+        }
+    }
+}
--- ParallelProgramming/ParallelProgramming.ParallelLINQ/Program.cs
+++ ParallelProgramming/ParallelProgramming.ParallelLINQ/Program.cs
@@ -1,8 +1,12 @@
 ﻿using ParallelProgramming.ParallelLINQ.Examples.AsParallelAndParallelQuery;
 using ParallelProgramming.ParallelLINQ.Examples.CancellationAndExceptions;
+using ParallelProgramming.ParallelLINQ.Examples.MergeOptions;
 
 Console.WriteLine($"\n{nameof(AsParallelAndParallelQuery)}\n");
 AsParallelAndParallelQuery.Start(args);
 
 Console.WriteLine($"\n{nameof(CancellationAndExceptions)}\n");
 CancellationAndExceptions.Start(args);
+
+Console.WriteLine($"\n{nameof(MergeOptions)}\n");
+MergeOptions.Start(args);
