c27736cc5cd6b8ec25a30e9685f05b6166acf0e8
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-11 18:55:27 +0200

    Additional Lectures: Duck Typing Mixins Added a new example demonstrating Duck Typing and Mixins.
    The example was part of the additional lecture series and offers a live coding demo to help understand these concepts better. The files were included in the DesignPatterns.Additional project, so it was necessary to update the solution file as well. The solution file has been updated to include the new project.

---
+++ DesignPatterns.Additional/Examples/DuckTypingMixins.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+
+namespace DesignPatterns.Additional.Examples.DuckTypingMixins;
+
+ref struct Foo
+{
+    public void Dispose()
+    {
+        Console.WriteLine("Disposing Foo");
+    }
+}
+
+public interface IMyDisposable<T> : IDisposable
+{
+    void IDisposable.Dispose()
+    {
+        Console.WriteLine($"Disposing {typeof(T).Name}");
+    }
+}
+
+public class MyClass : IMyDisposable<MyClass>
+{
+    
+}
+
+
+public interface IScalar<T> : IEnumerable<T>
+{
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        yield return (T)this;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public class MyScalarClass : IScalar<MyScalarClass>
+{
+    public override string ToString() => $"{nameof(MyScalarClass)}";
+}
+
+public class DuckTypingMixins
+{
+    public static void Start(string[] args)
+    {
+        // duck typing
+        
+        // GetEnumerator() - foreach (IEnumerable<T>)
+        // Dispose() - using (IDisposable)
+        using var foo = new Foo();
+
+        // mixin
+        using var mc = new MyClass();
+
+        var msc = new MyScalarClass();
+        foreach (var x in msc)
+            Console.WriteLine(x);
+    }
+}
---
+++ DesignPatterns.Additional/Program.cs
@@ -0,0 +1,4 @@
+﻿using DesignPatterns.Additional.Examples.DuckTypingMixins;
+
+Console.WriteLine($"\n{nameof(DuckTypingMixins)}\n");
+DuckTypingMixins.Start(args);
