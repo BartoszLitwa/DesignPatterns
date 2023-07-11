3898ad95ef12956bf405959c5431e2967af0b34e
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-11 19:10:01 +0200

    Additional Lectures: ASCII C# string Added a new example of ASCII C# string to the additional lectures.
    The new example demonstrates how to use ASCII string in C# through a new 'str' class and a new ASCII_C__String class. The 'Program.cs' file has also been updated to initiate this new example. The 'str' class includes various methods such as implicit conversion to ASCII string, equals function, and indexer for character access.

---
+++ DesignPatterns.Additional/Examples/ASCII_C#_String.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DesignPatterns.Additional.Examples.ASCII_C__String;
+
+// ascii utf-16
+public class str : IEquatable<str>, IEquatable<string>
+{
+    [NotNull] protected readonly byte[] buffer;
+
+    public str()
+    {
+        buffer = Array.Empty<byte>();
+    }
+
+    public str(string s)
+    {
+        buffer = Encoding.ASCII.GetBytes(s);
+    }
+
+    protected str(byte[] buffer)
+    {
+        this.buffer = buffer;
+    }
+
+    public override string ToString() => Encoding.ASCII.GetString(buffer);
+
+    public static implicit operator str(string s) => new(s);
+
+    public bool Equals(str other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return ((IStructuralEquatable)buffer).Equals(other.buffer, StructuralComparisons.StructuralEqualityComparer);
+    }
+
+    public bool Equals(string? other) => other == ToString();
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((str)obj);
+    }
+
+    public override int GetHashCode() => ToString().GetHashCode();
+
+    public static str operator +(str first, str second)
+    {
+        var bytes = new byte[first.buffer.Length + second.buffer.Length];
+        first.buffer.CopyTo(bytes, 0);
+        second.buffer.CopyTo(bytes, first.buffer.Length);
+        return new str(bytes);
+    }
+
+    public char this[int index]
+    {
+        get => (char)buffer[index];
+        set => buffer[index] = (byte)value;
+    }
+}
+
+public class ASCII_C__String
+{
+    public static void Start(string[] args)
+    {
+        
+    }
+}
--- DesignPatterns.Additional/Program.cs
+++ DesignPatterns.Additional/Program.cs
@@ -1,4 +1,8 @@
-﻿using DesignPatterns.Additional.Examples.DuckTypingMixins;
+﻿using DesignPatterns.Additional.Examples.ASCII_C__String;
+using DesignPatterns.Additional.Examples.DuckTypingMixins;
 
 Console.WriteLine($"\n{nameof(DuckTypingMixins)}\n");
 DuckTypingMixins.Start(args);
+
+Console.WriteLine($"\n{nameof(ASCII_C__String)}\n");
+ASCII_C__String.Start(args);
