b5585b51ffffca70cb07d9768de1f767a250ccf0
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-12 22:55:20 +0200

    Additional Lectures: Continuation Passing Style

---
+++ DesignPatterns.Additional/Examples/ContinuationPassingStyle.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+
+namespace DesignPatterns.Additional.Examples.ContinuationPassingStyle
+{
+    public enum WorkflowResult
+    {
+        Success, Failure
+    }
+
+    public class QuadraticEquationSolver
+    {
+        // ax^2 + bx + c == 0
+        public WorkflowResult Start(double a, double b, double c, out Tuple<Complex, Complex> result)
+        {
+            var disc = b * b - 4 * a * c;
+            if (disc < 0)
+            {
+                //result = SolveComplex(a, b, disc);
+                result = null;
+                return WorkflowResult.Failure;
+            }
+            else
+            {
+                return SolveSimple(a, b, disc, out result);
+            }
+        }
+
+        private WorkflowResult SolveComplex(double a, double b, double disc,
+            out Tuple<Complex, Complex> result)
+        {
+            var rootDisc = Complex.Sqrt(new Complex(disc, 0));
+            result = Tuple.Create(
+                (-b + rootDisc) / (2 * a),
+                (-b - rootDisc) / (2 * a)
+            );
+            return WorkflowResult.Failure;
+        }
+
+        private WorkflowResult SolveSimple(double a, double b, double disc,
+            out Tuple<Complex, Complex> result)
+        {
+            var rootDisc = Math.Sqrt(disc);
+            result = Tuple.Create(
+                new Complex((-b + rootDisc) / (2 * a), 0),
+                new Complex((-b - rootDisc) / (2 * a), 0)
+            );
+            return WorkflowResult.Success;
+        }
+    }
+
+    public class ContinuationPassingStyle
+    {
+        public static void Start(string[] args)
+        {
+            var solver = new QuadraticEquationSolver();
+            Tuple<Complex, Complex> solution;
+            var flag = solver.Start(1, 10, 16, out solution);
+        }
+    }
+}
--- DesignPatterns.Additional/Program.cs
+++ DesignPatterns.Additional/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesignPatterns.Additional.Examples.ASCII_C__String;
+using DesignPatterns.Additional.Examples.ContinuationPassingStyle;
 using DesignPatterns.Additional.Examples.DuckTypingMixins;
 
 Console.WriteLine($"\n{nameof(DuckTypingMixins)}\n");
@@ -6,3 +7,6 @@ DuckTypingMixins.Start(args);
 
 Console.WriteLine($"\n{nameof(ASCII_C__String)}\n");
 ASCII_C__String.Start(args);
+
+Console.WriteLine($"\n{nameof(ContinuationPassingStyle)}\n");
+ContinuationPassingStyle.Start(args);
