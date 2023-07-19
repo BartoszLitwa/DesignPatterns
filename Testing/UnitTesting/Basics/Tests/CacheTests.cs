38fbd175d4b55654f60b93caa969e1bfe22adeb2
BartoszLitwa <bartosz.litwa@proton.me>
2023-07-19 18:46:31 +0200

    Unit Testing Basics

---
+++ Testing/UnitTesting/Basics/Tests/CacheTests.cs
@@ -0,0 +1,40 @@
+﻿using Basics.Units;
+using Xunit;
+
+namespace Basics.Tests
+{
+    public class CacheTests
+    {
+        [Fact]
+        public void CachesItemWIthinTimeSpan()
+        {
+            var cache = new Cache(TimeSpan.FromDays(1));
+            cache.Add(new("url", "content", DateTime.Now));
+
+            var contains = cache.Contains("url");
+
+            Assert.True(contains);
+        }
+
+        [Fact]
+        public void Contains_ReturnsFalse_WhenOutsideTimeSpan()
+        {
+            var cache = new Cache(TimeSpan.FromDays(1));
+            cache.Add(new("url", "content", DateTime.Now.AddDays(-2)));
+
+            var contains = cache.Contains("url");
+
+            Assert.False(contains);
+        }
+
+        [Fact]
+        public void Contains_ReturnsFalse_WhenDoesntContainItem()
+        {
+            var cache = new Cache(TimeSpan.FromDays(1));
+
+            var contains = cache.Contains("url");
+
+            Assert.False(contains);
+        }
+    }
+}
---
+++ Testing/UnitTesting/Basics/Tests/ToDoListTests.cs
@@ -0,0 +1,52 @@
+﻿using Basics.Units;
+using Xunit;
+
+namespace Basics.Tests
+{
+    public class ToDoListTests
+    {
+        [Fact]
+        public void Add_SavesTodoItem()
+        {
+            // arrange
+            var list = new TodoList();
+
+            // act
+            list.Add(new("Test Content"));
+
+            // assert
+            var savedItem = Assert.Single(list.All);
+            Assert.NotNull(savedItem);
+            Assert.Equal(1, savedItem.Id);
+            Assert.Equal("Test Content", savedItem.Content);
+            Assert.False(savedItem.Complete);
+        }
+
+        [Fact]
+        public void TodoItem_Id_Increments_Every_Time_We_Add()
+        {
+            var list = new TodoList();
+
+            list.Add(new("Test Content 1"));
+            list.Add(new("Test Content 2"));
+
+            var items = list.All.ToArray();
+            Assert.Equal(1, items[0].Id);
+            Assert.Equal(2, items[1].Id);
+        }
+
+        [Fact]
+        public void Complete_Sets_TodoItem_Compelete_Flag_To_True()
+        {
+            var list = new TodoList();
+            list.Add(new("Test Content 1"));
+
+            list.Complete(1);
+
+            var completedItem = Assert.Single(list.All);
+            Assert.NotNull(completedItem);
+            Assert.Equal(1, completedItem.Id);
+            Assert.True(completedItem.Complete);
+        }
+    }
+}
